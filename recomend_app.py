5c2fe88c6462cc2152419a44cbeefe2761c1415d
sarrakabbous <sarra.kabbous@esprit.tn>
2024-07-29 11:54:07 +0200

    v2

--- recomend_app.py
+++ recomend_app.py
@@ -3,29 +3,29 @@ import pandas as pd
 import numpy as np
 import string
 import pickle
-df = pd.read_csv('new.csv', on_bad_lines='skip', engine ='python')
-df = df.dropna()
+df1 = pd.read_csv('new.csv',error_bad_lines=False, engine ='python')
+df1 = df1.dropna()
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 tdif = TfidfVectorizer(stop_words='english')
 
-df['jobdescription'] = df['jobdescription'].fillna('')
+df1['jobdescription'] = df1['jobdescription'].fillna('')
 
-tdif_matrix = tdif.fit_transform(df['jobdescription'])
+tdif_matrix = tdif.fit_transform(df1['jobdescription'])
 
 tdif_matrix.shape
 
 from sklearn.metrics.pairwise import sigmoid_kernel
 
 cosine_sim = sigmoid_kernel(tdif_matrix, tdif_matrix)
-indices = pd.Series(df.index, index=df['jobtitle']).drop_duplicates()
+indices = pd.Series(df1.index, index=df1['jobtitle']).drop_duplicates()
 def get_recommendations(title, cosine_sim=cosine_sim):
   idx = indices[title]
   sim_scores = list(enumerate(cosine_sim[idx]))
   sim_scores = sorted(sim_scores, key=lambda X: X[1], reverse=True)
   sim_scores = sim_scores[1:16]
   tech_indices = [i[0] for i in sim_scores]
-  return df['jobtitle'].iloc[tech_indices]
+  return df1['jobtitle'].iloc[tech_indices]
 
 
 
